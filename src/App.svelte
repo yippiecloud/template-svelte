596e1734b62f391a3632e4065b3b54df0ffeaca3
Mike Bild <mike@mikebild.com>
2021-09-05 10:09:44 +0200

    update

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,7 @@
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>Visit the <a href="https://www.yippie.cloud/docs">Yippie Cloud documentation</a> to learn how to deploy Svelte apps.</p>
 </main>
 
 <style>
